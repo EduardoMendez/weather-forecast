90b5a14c1665f1fd211c2d0916e0d14ddd2b7ee3
EduardoMendez <eduu.mendez@gmail.com>
2019-10-13 16:55:12 -0300

    Web API implementation beginning.

---
+++ WeatherForecast/WeatherForecastAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WeatherForecast;
+
+namespace WeatherForecastAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private Galaxy _galaxy;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            InitGalaxy();
+        }
+
+        [HttpGet]
+        public dynamic GetYearsForecast(int years = 1)
+        {
+            var periods = new WeatherForecaster(_galaxy.Planets, _galaxy.Sun).
+                GetWeatherPeriodsForecastForYears(years);
+
+            var droughtPeriods = periods.Count(p => p.Weather.IsDrought());
+            var rainnyPeriods = periods.Count(p => p.Weather.IsRain());
+            var optimalPeriods = periods.Count(p => p.Weather.IsOptimal());
+            var unknownPeriods = periods.Count(p => p.Weather.IsUnknown());
+
+            var peakDays = periods
+                .Where(p => p.HasPeakDay())
+                .Select(p => p.PeakDay);
+
+            var maxPeakLevel = peakDays.Max(d => d.CalculatePrecipitations());
+            var maxPeakDays = peakDays
+                .Where(pd => pd.CalculatePrecipitations() == maxPeakLevel)
+                .Select(d => d.Day);
+
+            return new { 
+                periodosDeSequia = droughtPeriods, 
+                periodosDeLluvia = rainnyPeriods,
+                periodosDeCondicionesOptimas = optimalPeriods,
+                periodosDeCondicionesDesconocidas = unknownPeriods,
+                diasPicoMaximoDeLluvia = maxPeakDays,
+                valorPicoMaximoDeLluvia = maxPeakLevel
+            };
+        }
+
+        private void InitGalaxy()
+        {
+            _galaxy = new Galaxy();
+
+            var ferengi = new Planet("ferengi", 1, Direction.CLOCKWISE, 500);
+            var vulcano = new Planet("vulcano", 5, Direction.COUNTERCLOCKWISE, 1000);
+            var betasoide = new Planet("betasoide", 3, Direction.CLOCKWISE, 2000);
+
+            _galaxy.Planets = new List<Planet>()
+            {
+                ferengi, vulcano, betasoide
+            };
+
+            _galaxy.Sun = new Point(0, 0);
+        }
+    }
+}
---
+++ WeatherForecast/WeatherForecastAPI/Galaxy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeatherForecast;
+
+namespace WeatherForecastAPI
+{
+    public class Galaxy
+    {
+        public IEnumerable<Planet> Planets { get; set; }
+        public Point Sun { get; set; }
+
+    }
+}
